2329076fa41f37970559fe520e1f6ec1f217e346
Ben <feba23@live.com>
2024-04-19 01:51:31 -0500

    Initial svelte commit

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
         },
         paths: {
 
-            base: process.env.NODE_ENV === 'production' ? '/benbarcaskey.com' : '',
+            base: process.argv.includes('dev') ? '' : '/Slad3.github.io'
 
         }
     },
