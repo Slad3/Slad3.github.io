103ed04398f1987dc3d858536acd3740e36ab832
Ben <feba23@live.com>
2024-04-19 00:53:40 -0500

    Updating package.json

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.benbarcaskey.www',
+  appName: 'benbarcaskey',
+  webDir: 'dist/benbarcaskey',
+  bundledWebRuntime: false
+};
+
+export default config;
